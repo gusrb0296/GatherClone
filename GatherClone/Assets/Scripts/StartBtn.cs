fb473c4afc2fbdf9014ec080aecda12b63500add
GoHyunkyu <gusrb0296@naver.com>
2023-11-28 20:12:45 +0900

    Add StartScene

---
+++ GatherClone/Assets/Scripts/StartBtn.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartBtn : MonoBehaviour
+{
+    public TMP_InputField PlayerNameInput;
+    private string _playerName = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _playerName = PlayerNameInput.text;
+    }
+
+    public void InputName()
+    {
+        _playerName = PlayerNameInput.text;
+        PlayerPrefs.SetString("CurrentPlayerName", _playerName);
+        Debug.Log(_playerName);
+    }
+
+
+    public void GameStart()
+    {
+        Debug.Log(_playerName);
+        if (_playerName.Length > 0)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        
+    }
+}
